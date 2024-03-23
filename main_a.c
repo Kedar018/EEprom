5df9eb45664833aa6011fe8c48bbab0cc54e13b4
Kedar018 <164355890+Kedar018@users.noreply.github.com>
2024-03-23 18:14:39 +0530

    Add files via upload

--- main_a.c
+++ main_a.c
@@ -2,15 +2,14 @@
 #include"head.h"
 main()
 {
-  char p[1000];
-  char a[1000];
+  char p[10];
+  char a[10];
   int page[]={0XA0,0XA2,0XA4,0XA6},c=0;
   int i=0,j=0;
   i2c_init();
-  uart0_init(9600);
+  uart0_init(2400);
   uart0_tx_string("start\r\n");	
-  uart0_rx_string(a,1000); 
-  while(uart0_rx()!='\r');	
+  uart0_rx_string(a,10); 
   for(i=0;a[i];i++)
    {
       i2c_bytes_write_frame(page[c],0x00+j,a[i]);
@@ -37,5 +36,5 @@ uart0_tx_string("half done\r\n");
  }
 p[i]='\0';
 uart0_tx_string(p);
-uart0_tx_string("done\r\n");
+uart0_tx_string("\r\ndone");
 }
